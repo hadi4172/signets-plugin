515a0960289b21d67379aa8849a01cb8891987d0
Antoine Boucher <antoine.bou13@gmail.com>
2023-03-27 13:02:42 -0400

    move into vendors and change readme

--- popup.js
+++ popup.js
@@ -1,67 +1,30 @@
-window.onload = function () {
-    let checkboxNoColors = document.querySelector("#disablecolors");
-    let checkboxGPAInNumber = document.querySelector("#gpainnumber");
-    let checkboxShowGrades = document.querySelector("#alwaysshowgrades");
-    let checkboxPreciseGrades = document.querySelector("#precisegrades");
-    let radioThemes = Array.from(document.querySelectorAll('[name="theme"]'));
-    let estimerCoteBtn = document.querySelector("#btn-estimer-cote");
-
-    chrome.storage.sync.get(['noColors', 'showGrades', 'theme', 'preciseGrades', 'gpaInNumber'], function (arg) {
-        checkboxNoColors.checked = false;
-        if (typeof arg.noColors !== 'undefined') {
-            if (arg.noColors === true) checkboxNoColors.checked = true;
-        }
-
-        checkboxGPAInNumber.checked = false;
-        if (typeof arg.gpaInNumber !== 'undefined') {
-            if (arg.gpaInNumber === true) checkboxGPAInNumber.checked = true;
-        }
-
-        checkboxShowGrades.checked = false;
-        if (typeof arg.showGrades !== 'undefined') {
-            if (arg.showGrades === true) checkboxShowGrades.checked = true;
-        }
-
-        checkboxPreciseGrades.checked = false;
-        if (typeof arg.preciseGrades !== 'undefined') {
-            if (arg.preciseGrades === true) checkboxPreciseGrades.checked = true;
-        }
-
-        if (typeof arg.theme !== 'undefined') {
-            for (let radioTheme of radioThemes) {
-                if (radioTheme.getAttribute("value") === arg.theme) radioTheme.checked = true;
-            }
-        } else {
-            chrome.storage.sync.set({ theme: "default-theme" });
-            radioThemes[0].checked = true;
-        }
-
+window.addEventListener('load', () => {
+    const checkboxNoColors = document.querySelector('#disablecolors');
+    const checkboxGPAInNumber = document.querySelector('#gpainnumber');
+    const checkboxShowGrades = document.querySelector('#alwaysshowgrades');
+    const checkboxPreciseGrades = document.querySelector('#precisegrades');
+    const radioThemes = Array.from(document.querySelectorAll('[name="theme"]'));
+    const estimerCoteBtn = document.querySelector('#btn-estimer-cote');
+  
+    chrome.storage.sync.get(
+      ['noColors', 'showGrades', 'theme', 'preciseGrades', 'gpaInNumber'],
+      ({ noColors, showGrades, theme, preciseGrades, gpaInNumber }) => {
+        checkboxNoColors.checked = noColors ?? false;
+        checkboxGPAInNumber.checked = gpaInNumber ?? false;
+        checkboxShowGrades.checked = showGrades ?? false;
+        checkboxPreciseGrades.checked = preciseGrades ?? false;
+        
+        const selectedTheme = radioThemes.find(theme => theme.value === theme);
+        selectedTheme?.setAttribute('checked', true) ?? radioThemes[0].setAttribute('checked', true);
     });
-    checkboxNoColors.addEventListener("click", function () {
-        chrome.storage.sync.set({ noColors: checkboxNoColors.checked });
-    });
-
-    checkboxGPAInNumber.addEventListener("click", function () {
-        chrome.storage.sync.set({ gpaInNumber: checkboxGPAInNumber.checked });
-    });
-
-    checkboxShowGrades.addEventListener("click", function () {
-        chrome.storage.sync.set({ showGrades: checkboxShowGrades.checked });
-    });
-
-    checkboxPreciseGrades.addEventListener("click", function () {
-        chrome.storage.sync.set({ preciseGrades: checkboxPreciseGrades.checked });
-    });
-
-    for (let radioTheme of radioThemes) {
-        radioTheme.addEventListener("click", function () {
-            chrome.storage.sync.set({ theme: radioTheme.getAttribute("value") });
-        });
-    }
-
-    estimerCoteBtn.addEventListener("click", function () {
-        chrome.windows.create({ 'url': 'estimerCote.html', 'type': 'popup', 'width': 300, 'height': 190 }, function (window) {
-        });
-    });
-
-};
+  
+    const updateChromeStorage = (key, value) => chrome.storage.sync.set({ [key]: value });
+    
+    checkboxNoColors.addEventListener('click', () => updateChromeStorage('noColors', checkboxNoColors.checked));
+    checkboxGPAInNumber.addEventListener('click', () => updateChromeStorage('gpaInNumber', checkboxGPAInNumber.checked));
+    checkboxShowGrades.addEventListener('click', () => updateChromeStorage('showGrades', checkboxShowGrades.checked));
+    checkboxPreciseGrades.addEventListener('click', () => updateChromeStorage('preciseGrades', checkboxPreciseGrades.checked));
+    radioThemes.forEach(theme => theme.addEventListener('click', () => updateChromeStorage('theme', theme.value)));
+    
+    estimerCoteBtn.addEventListener('click', () => chrome.windows.create({ url: 'estimerCote.html', type: 'popup', width: 300, height: 190 }));
+  });  
